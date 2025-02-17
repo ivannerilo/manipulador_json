acbd2adaf838b67ea7188e047eda397b6bf02f28
ivannerilo <ivannerilo05@gmail.com>
2025-02-17 11:53:50 -0300

    Adicionando a função de edição, ainda não completa

--- manipulador/util.py
+++ manipulador/util.py
@@ -11,6 +11,44 @@ def json_file_size():
         jsonfiles = list(f)
         return len(jsonfiles)
 
+def formatador_html(lista, json):
+    for messages in json["messages"][1:]:
+        if isinstance(messages["content"], list):
+            content =  f"""
+                <div>
+                    <p><strong>{messages["role"]}</strong></p>
+                    <textarea rows="10", cols="220">{messages["content"][0]["text"]}</textarea>
+                </div>
+            """
+            lista.append(content)
+        else:
+            content =  f"""
+                <div>
+                    <p><strong>{messages["role"]}</strong></p>
+                    <textarea rows="10", cols="220">{messages["content"]}</textarea>
+                </div>
+            """
+            lista.append(content)
+    return lista
+
+
+def formatador_formulario(lista, json):
+    id = 0
+    for messages in json["messages"]:
+        if isinstance(messages["content"], list):
+            content =  f"""
+                <p><strong>{messages["role"]}</strong></p>
+                <textarea name="{id}" rows="10", cols="220">{messages["content"][0]["text"]}</textarea>
+            """
+            lista.append(content)
+        else:
+            content =  f"""
+                <p><strong>{messages["role"]}</strong></p>
+                <textarea name="{id}"  rows="10", cols="220">{messages["content"]}</textarea>
+            """
+            lista.append(content)
+        id += 1
+    return lista
 
 
 
--- manipulador/views.py
+++ manipulador/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django import forms
 from . import util
 
 DEFAULT_ARCHIVE_NUM = 0
+
+class editForm(forms.Form):
+    new_content = forms.CharField(
+		widget=forms.Textarea(attrs={'rows': 10, 'cols': 220}),
+	)
 
 # Create your views here.
 def index(request):
@@ -20,13 +26,26 @@ def show_json(request, archive_num):
         if num < 0 or num > file_size -1:
             num = 0
         return HttpResponseRedirect(f'/{num}')
-    return render(request, 'manipulador/index.html', {
-        "json": util.json_reader(archive_num)
+
+    correct_content = []
+    json = util.json_reader(archive_num)
+    correct_content = util.formatador_html(correct_content, json)
+    return render(request, "manipulador/index.html", {
+        "correct_content": correct_content
     })
+        
 
 def edit_json(request, archive_num):
-    return render(request, 'manipulador/edit.html', {
-        "json": util.json_reader(archive_num)
+    if request.method == 'POST':
+        return render(request, "manipulador/teste.html", {
+            "teste": request.POST
+        })
+
+    correct_form = []
+    json = util.json_reader(archive_num)
+    correct_form = util.formatador_formulario(correct_form, json)
+    return render(request, "manipulador/edit.html", {
+        "correct_form": correct_form
     })
 
 
